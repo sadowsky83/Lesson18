759200caf79ebe0b70f566a22d0509ac7382802f
Anatoly Sadowsky <sadowsky83@gmail.com>
2021-12-27 21:16:39 +0700

    Добавьте файлы проекта.

---
+++ task1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    internal class Program
+    {
+        // Строка со скобками
+        // Строка для проверки не правильная 123}9876516(2){$!$%sgsegg)waarafa}644sfa{6454qF ff]sdfsf
+        // Строка для проверки правильная 123(98765162{$!$%sgsegg}waarafa)644sfa[6454qF ff]sdfsf
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку, содержащую скобки (круглые (), квадратные [] и фигурные {}) и другие символы:");            
+            string str = Console.ReadLine();
+            Console.WriteLine(StrCheck(str) ? "В введенной строке - скобки расставлены правильно." : "В введенной строке - скобки расставлены не правильно!");
+            Console.ReadKey();
+        }
+        static bool StrCheck(string str)
+        {
+            Stack<char> stak = new Stack<char>();
+            foreach (var s in str)
+            {
+                if (s == '[')
+                    stak.Push(']');
+                if (s == '(')
+                    stak.Push(')');
+                if (s == '{')
+                    stak.Push('}');
+                if (stak.Count != 0 && s == stak.Peek())
+                    stak.Pop();
+            }
+            if (stak.Count != 0)
+                return false;
+            return true;
+        }
+    }
+}
